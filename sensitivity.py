Fixed an error when using energies of TeV units for extrapolation of energies in ergs units

sensitivity.py
@@ -2,13 +2,11 @@ import numpy as np
 from scipy.interpolate import RegularGridInterpolator
 import pandas as pd
 
-
-
 class HAWCSensi:
     '''
 
     '''
-    energy = [0.17782794,0.56234133,1.77827941,5.62341325,17.7827941,56.23413252] #in TeV
+    energy = np.array([0.17782794,0.56234133,1.77827941,5.62341325,17.7827941,56.23413252])*1.6 #in TeV
     zenith = [0.,10.,20.,30.,40.,50.] #degrees
     time = [np.log10(1.0),np.log10(100.)]
     zenith_name = ["0deg","10deg","20deg","30deg","40deg","50deg"]
@@ -28,13 +26,13 @@ class HAWCSensi:
         df.columns = ["50deg","40deg","30deg","20deg","10deg","0deg"] #it is inverted respect to self.zenith_name, thats why I need to "rewrite" this one here
 
         df["Energy"] = self.energy #units of TeV
-        df["Emin"] = [0.1,0.31622777,1.,3.16227766,10.,31.6227766] #units of TeV
-        df["Emax"] = [0.31622777,1.,3.16227766,10.,31.6227766,100.] #units of TeV
+        df["Emin"] = np.array([0.1,0.31622777,1.,3.16227766,10.,31.6227766])*1.6 #units of TeV
+        df["Emax"] = np.array([0.31622777,1.,3.16227766,10.,31.6227766,100.])*1.6 #units of TeV
 
         #Since these are values in E2dnde erg cm-2 s-1 and I want dnde
         for icol in ["50deg","40deg","30deg","20deg","10deg","0deg"]:
             #the two 1.602 is to convert the energy from TeV to erg
-            df[icol] = df[icol]/ (df["Energy"]*df["Energy"]*1.602*1.602) #now we will have units of erg-1 cm-2 s-1
+            df[icol] = df[icol]/ (df["Energy"]*df["Energy"]) #now we will have units of erg-1 cm-2 s-1
 
         df["Edown"] = df["Energy"]-df["Emin"]
         df["Eup"] = df["Emax"]-df["Energy"]
@@ -69,6 +67,9 @@ class HAWCSensi:
         #For your given zenith
         #I make here a simple linear scaling between (x1,y1) ie sensi at energy E,1second
         #and sensi at (x2,y2) ie sensi at E and time 100 seconds
+        #convert the input energy to ergs since all the interpolation functions use ergs
+
+        e = e*1.6
         y1 = np.log10(self.sensi_interpolate([z,e,self.time[0]])[0])
         y2 =np.log10(self.sensi_interpolate([z,e,self.time[1]])[0])
         x1, x2 = self.time[0],self.time[1]
@@ -79,22 +80,29 @@ class HAWCSensi:
 
         return np.power(10,y)
 
-    def sensitivity_vs_time(self,zenith, emin, emax, times):
+    def sensitivity_vs_time(self,zenith, emin, emax, times ):
+
         #"times" is a numpy array of times
         #eg using tc:
         #ti = 10**np.arange(-1,3.5,.5)
         #tf = 10**np.arange(-.5,4,.5)
         #tc = (tf+ti)/2
+        emin = emin*1.6
+        emax = emax*1.6
+
         nbins = 30
-        e_bins = np.logspace(np.log10(emin),np.log10(emax),nbins) #in TeV
+        e_bins = np.logspace(np.log10(emin),np.log10(emax),nbins) #in ergs
 
         s_vs_time = []
+
         for it in times:
             per_time = []
             #here we apply a simple geometrical integration of the curve \int_emin^_emax S(E)xExdE where S(E) is the sensitivity
             for ie in e_bins:
-                per_time.append(self.interpolate(zenith,ie,it)*ie*1.6) #convert to erg
-            s_vs_time.append(np.sum( np.array(per_time)* (emax-emin)*1.6/float(nbins))    ) #convert to erg
+                #intperolate gives dnde, multiply by E to have Ednde
+                per_time.append((self.interpolate(zenith,ie/1.6,it))*ie)
+            #we do the sum of the numerical integration here
+            s_vs_time.append(np.sum( np.array(per_time)* (emax-emin)/float(nbins))    ) #this integration will give an additional energy unit to have finaly E2dnde
 
         #will return an array of lenght equalt to the times array
         return s_vs_time